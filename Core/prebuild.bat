6c95c4f863e6e074d947c978c8caeb4381dde0ff
guoxiaoxin <guoxx@me.com>
2016-01-04 17:33:58 +0800

    use batch file for prebuild event

---
+++ Core/prebuild.bat
@@ -0,0 +1,18 @@
+SET SolutionDir=%1
+SET ProjectDir=%2
+SET OutDir=%3
+
+@ECHO ON
+ECHO "prebuild start"
+
+ECHO "COPY render doc dll to "%OutDir%
+COPY %SolutionDir%"ThirdParty\RenderDoc\x86\*" %OutDir%
+IF NOT EXIST %OutDir%"renderdoc" MKDIR %OutDir%"renderdoc"
+
+ECHO "COPY shaders to "%OutDir%
+COPY %ProjectDir%"Shaders\*" %OutDir%
+
+ECHO "COPY resources to "%OutDir%
+COPY %SolutionDir%"Resources\*" %OutDir%
+
+ECHO "prebuild end"
