a0a290de87a15d261d188ee245b5afc7264f1621
guoxiaoxin <guoxx@me.com>
2016-01-07 13:38:51 +0800

    bug fix

--- Core/Shaders/BaseMaterial_ps.hlsl
+++ Core/Shaders/BaseMaterial_ps.hlsl
@@ -7,7 +7,7 @@ PSOutput main(PSInput input)
 	float d = length(lightPositionInCameraSpace.xyz - input.positionCS);
 	float3 L = normalize(lightPositionInCameraSpace.xyz - input.positionCS);
 	float3 V = normalize(-input.positionCS);
-	float3 H = (V + L) / 2;
+	float3 H = normalize((V + L) / 2);
 	float3 N = normalize(input.normalCS);
 	float3 E = pointLightIrradiance(intensity.xyz, d, radiusStart, radiusEnd);
 	float cosineTheaI = max(0, dot(N, L));
@@ -15,7 +15,7 @@ PSOutput main(PSInput input)
 	float3 diffuseLight = (E * cosineTheaI / C_PI) * diffuse.xyz;
 	float specularPower = shininess;
 	float3 specularLight = (specularPower + 8.0) / (8.0 * C_PI) * pow(cosineTheaH, specularPower) * E * specular.xyz;
-	result.color.rbg = specularLight;
+	result.color.rgb = diffuseLight + specularLight;
 	result.color.a = 1.0;
 	return result;
 }
