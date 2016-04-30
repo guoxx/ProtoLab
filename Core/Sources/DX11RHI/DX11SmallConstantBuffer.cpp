c2a0489132935d6643b5b8213a395211ce5856d9
guoxiaoxin <guoxx@me.com>
2016-05-01 00:09:08 +0800

    Add helper functions for small constant buffer.

--- Core/Sources/DX11RHI/DX11SmallConstantBuffer.cpp
+++ Core/Sources/DX11RHI/DX11SmallConstantBuffer.cpp
@@ -14,6 +14,12 @@ DX11SmallConstantBuffer::~DX11SmallConstantBuffer()
 	_aligned_free(_cache);
 }
 
+void DX11SmallConstantBuffer::setVectorF(uint32_t reg, float f0, float f1, float f2, float f3)
+{
+	DirectX::XMVECTOR vec{ f0, f1, f2, f3 };
+	setVectorF(reg, vec);
+}
+
 void DX11SmallConstantBuffer::setVectorF(uint32_t reg, DirectX::FXMVECTOR vec)
 {
 	assert((reg * sizeof(float4) + sizeof(float4)) <= _bufferSizeInBytes);
@@ -21,11 +27,23 @@ void DX11SmallConstantBuffer::setVectorF(uint32_t reg, DirectX::FXMVECTOR vec)
 	DirectX::XMStoreFloat4(ptr, vec);
 }
 
+void DX11SmallConstantBuffer::setVectorI(uint32_t reg, int32_t i0, int32_t i1, int32_t i2, int32_t i3)
+{
+	DirectX::XMVECTORI32 vec{ i0, i1, i2, i3 };
+	setVectorI(reg, vec);
+}
+
 void DX11SmallConstantBuffer::setVectorI(uint32_t reg, DirectX::XMVECTORI32 vec)
 {
 	assert((reg * sizeof(float4) + sizeof(int4)) <= _bufferSizeInBytes);
 	int4* ptr = reinterpret_cast<int4*>(_cache + reg);
 	DirectX::XMStoreSInt4(ptr, vec);
+}
+
+void DX11SmallConstantBuffer::setVectorU(uint32_t reg, uint32_t u0, uint32_t u1, uint32_t u2, uint32_t u3)
+{
+	DirectX::XMVECTORU32 vec{ u0, u1, u2, u3 };
+	setVectorU(reg, vec);
 }
 
 void DX11SmallConstantBuffer::setVectorU(uint32_t reg, DirectX::XMVECTORU32 vec)
--- Core/Sources/DX11RHI/DX11SmallConstantBuffer.h
+++ Core/Sources/DX11RHI/DX11SmallConstantBuffer.h
@@ -6,9 +6,15 @@ public:
 	DX11SmallConstantBuffer(DX11Device* dev, void* mem, uint32_t sizeInBytes);
 	~DX11SmallConstantBuffer();
 
+	void setVectorF(uint32_t reg, float f0 = 0.0f, float f1 = 0.0f, float f2 = 0.0f, float f3 = 0.0f);
 	void setVectorF(uint32_t reg, DirectX::FXMVECTOR vec);
+
+	void setVectorI(uint32_t reg, int32_t i0 = 0, int32_t i1 = 0, int32_t i2 = 0, int32_t i3 = 0);
 	void setVectorI(uint32_t reg, DirectX::XMVECTORI32 vec);
+
+	void setVectorU(uint32_t reg, uint32_t u0 = 0, uint32_t u1 = 0, uint32_t u2 = 0, uint32_t u3 = 0);
 	void setVectorU(uint32_t reg, DirectX::XMVECTORU32 vec);
+
 	void setMatrix(uint32_t reg, DirectX::FXMMATRIX matrix);
 
 	void commit(DX11GraphicContext* ctx);
