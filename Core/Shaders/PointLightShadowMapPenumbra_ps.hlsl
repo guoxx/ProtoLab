9b40ff115acb42855311bf49645480b4cc48d71b
guoxiaoxin <guoxx@me.com>
2016-05-12 01:00:45 +0800

    draft

--- Core/Shaders/PointLightShadowMapPenumbra_ps.hlsl
+++ Core/Shaders/PointLightShadowMapPenumbra_ps.hlsl
@@ -1,5 +1,16 @@
 #include "PointLightShadowMapPenumbra_common.h"
 
+TextureCube<float> g_PointLightShadowMap : register(t0);
+TextureCube<float> g_PointLightDilatedShadowMap : register(t1);
+Texture2D<float> g_DepthBuffer : register(t2);
+
+SamplerState g_PointSampler : register(s0);
+
+float EstimatePenumbra(float zReceiver, float zBlocker)
+{
+	return (zReceiver - zBlocker) / zBlocker;
+}
+
 float4 main(VSOutput input) : SV_TARGET
 {
 	return float4(1, 1, 1, 1);
