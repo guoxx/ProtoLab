7a032b5b94d5f5d88201d7121799f28951b45242
guoxiaoxin <guoxx@me.com>
2016-05-24 00:23:30 +0800

    draft

--- Core/Shaders/PointLightShadowMapBlur_ps.hlsl
+++ Core/Shaders/PointLightShadowMapBlur_ps.hlsl
@@ -1,6 +1,15 @@
 #include "PointLightShadowMapBlur_common.h"
 
+
 SamplerState g_PointSampler : register(s0);
+
+float2 get_step_size( float2 direction, float3 normal, float depth, float threshold )
+{
+  return direction 
+         //* light_size * light_size //included in the penumbra
+         * sqrt( max( dot( float3( 0, 0, 1 ), normal ), threshold ) )
+         * (1 / (depth/* * depth * 100*/));
+}
 
 float4 main(VSOutput input) : SV_TARGET
 {
